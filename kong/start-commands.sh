c3a8cd36f4c6ae6e0fcd367110a83b7873976268
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-04-21 22:57:23 +0200

    fix

--- kong/start-commands.sh
+++ kong/start-commands.sh
@@ -12,14 +12,21 @@ curl -s -X POST http://localhost:8001/services \
     -d name=register-api \
     -d url=http://register-api:8080 \
     | python3 -mjson.tool
-# Now take ID! like: a42388a5-25ee-4297-9398-b4808b84ccef
+# Now take ID! like: 16f5ba59-2a2f-4378-bd7c-f3df1e6475e3
 
-curl -s -X DELETE http://localhost:8001/routes \
-    -d service.id=a42388a5-25ee-4297-9398-b4808b84ccef \
+curl -s -X POST http://localhost:8001/routes \
+    -d service.id=16f5ba59-2a2f-4378-bd7c-f3df1e6475e3 \
     -d paths[]=/ \
     | python3 -mjson.tool
 
-#and you will get ID like: 55e9cc40-2a6b-4ba0-bc08-42dd68134f39
+#and you will get ID like: aca8e64c-c6bd-4455-9712-b4ca6349079d
 
 # If you want to relete:
 #curl -X DELETE "http://localhost:8001/services/register-api/routes/55e9cc40-2a6b-4ba0-bc08-42dd68134f39"
+
+curl -s -X POST http://localhost:8001/plugins \
+  -d name=oidc \
+  -d config.client_id=kong \
+  -d config.client_secret=8003ba0b-073a-4ccb-a4d4-c024064c2fac \
+  -d config.discovery=http://keycloak:8080/auth/realms/udomiljubimcadev/.well-known/openid-configuration \
+  | python3 -mjson.tool
