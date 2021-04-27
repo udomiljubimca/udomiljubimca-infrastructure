c37b3bf6819ba122b267642493c439e2131b5ea1
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-04-28 00:53:47 +0200

    small fix

--- kong/start-commands.sh
+++ kong/start-commands.sh
@@ -3,6 +3,8 @@ docker-compose -f docker-compose-kong.yaml up -d kong-db
 docker-compose -f docker-compose-kong.yaml run --rm kong kong migrations bootstrap
 docker-compose -f docker-compose-kong.yaml run --rm kong kong migrations up
 docker-compose -f docker-compose-kong.yaml up -d kong
+docker-compose -f docker-compose-kong.yaml up -d konga-gui
+
 
 # check if it's true
 curl -s http://localhost:8001 | jq .plugins.available_on_server.oidc
@@ -12,21 +14,24 @@ curl -s -X POST http://localhost:8001/services \
     -d name=register-api \
     -d url=http://register-api:8080 \
     | python3 -mjson.tool
-# Now take ID! like: 16f5ba59-2a2f-4378-bd7c-f3df1e6475e3
+# Now take ID! like: 86089e8b-297c-4065-85dc-ba97224614a0
 
 curl -s -X POST http://localhost:8001/routes \
-    -d service.id=16f5ba59-2a2f-4378-bd7c-f3df1e6475e3 \
+    -d service.id=86089e8b-297c-4065-85dc-ba97224614a0 \
     -d paths[]=/ \
     | python3 -mjson.tool
 
-#and you will get ID like: aca8e64c-c6bd-4455-9712-b4ca6349079d
+#and you will get ID like: 240dae85-70ab-4332-9f19-a3334a9a793b
 
 # If you want to relete:
 #curl -X DELETE "http://localhost:8001/services/register-api/routes/55e9cc40-2a6b-4ba0-bc08-42dd68134f39"
 
 curl -s -X POST http://localhost:8001/plugins \
   -d name=oidc \
-  -d config.client_id=kong \
+  -d config.client_id=app \
   -d config.client_secret=8003ba0b-073a-4ccb-a4d4-c024064c2fac \
-  -d config.discovery=http://keycloak:8080/auth/realms/udomiljubimcadev/.well-known/openid-configuration \
+  -d config.ssl_verify=false \
+  -d config.realm=udomiljubimcadev \
+  -d config.introspection_endpoint=http://149.81.126.136:8080/auth/realms/udomiljubimcadev/protocol/openid-connect/token/introspect \
+  -d config.discovery=http://149.81.126.136:8080/auth/realms/udomiljubimcadev/.well-known/openid-configuration \
   | python3 -mjson.tool
