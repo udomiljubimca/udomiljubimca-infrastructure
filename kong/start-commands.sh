8b7afb18688d5bc20a397441453453d59541abda
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-04-28 22:18:49 +0200

    fix ports

--- kong/start-commands.sh
+++ kong/start-commands.sh
@@ -24,10 +24,10 @@ curl -s -X POST http://localhost:8001/routes \
 # Set plugin to service
 # curl -s -X POST http://localhost:8001/services/6339f5f9-344c-4675-9ac5-689b0ed63276/plugins \
 #   -d name=jwt-keycloak \
-#   -d config.allowed_iss=http://dev.udomi-ljubimca.com:8080/auth/realms/udomiljubimcadev \
+#   -d config.allowed_iss=http://149.81.126.136:8080/auth/realms/udomiljubimcadev \
 #   | python3 -mjson.tool
 
 
 # GET TOKEN!
-curl -d "client_secret=<>" -d "client_id=app" -d "username=ognjenit" -d "password=Test123" -d "grant_type=password" "http://dev.udomi-ljubimca.com:8080/auth/realms/udomiljubimcadev/protocol/openid-connect/token"
+curl -d "client_secret=<>" -d "client_id=app" -d "username=ognjenit" -d "password=Test123" -d "grant_type=password" "http://149.81.126.136:8080/auth/realms/udomiljubimcadev/protocol/openid-connect/token"
 #################################################################################################################
