51419773a42b55cf917a60c0ca46d2e5e4c64fd4
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-04-28 22:10:16 +0200

    change port number

--- kong/start-commands.sh
+++ kong/start-commands.sh
@@ -13,21 +13,21 @@ curl -s -X POST http://localhost:8001/services \
     -d name=register-api \
     -d url=http://register-api:8080 \
     | python3 -mjson.tool
-# Now take ID! like: 2be71d46-cddd-4a1e-b3c4-bacc4a6cfe14
+# Now take ID! like: 6339f5f9-344c-4675-9ac5-689b0ed63276
 
 # Create route
 curl -s -X POST http://localhost:8001/routes \
-    -d service.id=2be71d46-cddd-4a1e-b3c4-bacc4a6cfe14 \
+    -d service.id=6339f5f9-344c-4675-9ac5-689b0ed63276 \
     -d paths[]=/ \
     | python3 -mjson.tool
 
 # Set plugin to service
-curl -s -X POST http://localhost:8001/services/2be71d46-cddd-4a1e-b3c4-bacc4a6cfe14/plugins \
-  -d name=jwt-keycloak \
-  -d config.allowed_iss=http://dev.udomi-ljubimca.com:8080/auth/realms/udomiljubimcadev \
-  | python3 -mjson.tool
+# curl -s -X POST http://localhost:8001/services/6339f5f9-344c-4675-9ac5-689b0ed63276/plugins \
+#   -d name=jwt-keycloak \
+#   -d config.allowed_iss=http://dev.udomi-ljubimca.com:8080/auth/realms/udomiljubimcadev \
+#   | python3 -mjson.tool
 
 
 # GET TOKEN!
-curl -d "client_secret=8003ba0b-073a-4ccb-a4d4-c024064c2fac" -d "client_id=app" -d "username=ognjenit" -d "password=Test123" -d "grant_type=password" "http://dev.udomi-ljubimca.com:8080/auth/realms/udomiljubimcadev/protocol/openid-connect/token"
+curl -d "client_secret=<>" -d "client_id=app" -d "username=ognjenit" -d "password=Test123" -d "grant_type=password" "http://dev.udomi-ljubimca.com:8080/auth/realms/udomiljubimcadev/protocol/openid-connect/token"
 #################################################################################################################
