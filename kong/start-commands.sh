45a62449b132780563410d51be86a08b7a6aaf02
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-04-29 08:12:13 +0200

    chnaged roules

--- kong/start-commands.sh
+++ kong/start-commands.sh
@@ -10,19 +10,26 @@ docker-compose -f docker-compose-kong.yaml up -d konga-gui
 ########################################### EXAMPLE FOR REGISTER API ###########################################
 # Create Service
 curl -s -X POST http://10.123.176.2:8001/services \
-    -d name=register-api \
+    -d name=register-api-service \
     -d url=http://register-api:8080 \
     | python3 -mjson.tool
-# Now take ID! like: 6339f5f9-344c-4675-9ac5-689b0ed63276
+# Now take ID! like: 1be5b32b-b7c1-464e-a1b9-b6ab14074c53
 
 # Create route
 curl -s -X POST http://10.123.176.2:8001/routes \
-    -d service.id=6339f5f9-344c-4675-9ac5-689b0ed63276 \
+    -d name=register-api-route \
+    -d service.id=1be5b32b-b7c1-464e-a1b9-b6ab14074c53 \
+    -d hosts[]=149.81.126.136 \
+    -d hosts[]=10.123.176.2 \
     -d paths[]=/ \
+    -d methods[]=GET \
+    -d methods[]=POST \
+    -d protocols[]=http \
+    -d protocols[]=https \
     | python3 -mjson.tool
 
 # Set plugin to service
-# curl -s -X POST http://10.123.176.2:8001/services/6339f5f9-344c-4675-9ac5-689b0ed63276/plugins \
+# curl -s -X POST http://10.123.176.2:8001/services/1be5b32b-b7c1-464e-a1b9-b6ab14074c53/plugins \
 #   -d name=jwt-keycloak \
 #   -d config.allowed_iss=http://149.81.126.136:8080/auth/realms/udomiljubimcadev \
 #   | python3 -mjson.tool
