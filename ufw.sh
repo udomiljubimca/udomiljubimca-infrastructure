1e0cdb785d1ad61dbdd650ecd43956710db4d91c
Ognjen Stanisavljevic <ognjen.stanisavljevic@thingsolver.com>
2021-04-30 20:59:20 +0200

    remove 8443 port and added comamnds for ssl no req

--- ufw.sh
+++ ufw.sh
@@ -8,3 +8,10 @@ ufw allow 80
 ufw allow 443
 ufw allow from any to 10.123.176.2 port 8080
 ufw allow from 104.248.249.114 to any port 
+
+
+# Set master url to use only http 
+docker exec -it keycloak /bin/bash
+/opt/jboss/keycloak/bin/
+./kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin
+./kcadm.sh update realms/master -s sslRequired=NONE
